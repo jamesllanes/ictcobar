ce0c8a3bcfa29f5ffa8963b2087ae260e0e7cc83
Unknown <jessie_james_llanes@dlsl.edu.ph>
2018-05-25 16:59:31 +0800

    change in connection

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,5 +1,8 @@
 <?php
-	$connect=mysqli_connect("localhost","root","") or die("Unable to connect");
+	/*$connect=mysqli_connect("localhost","root","") or die("Unable to connect");*/
+	
+	$connnect=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "{ictc-obar.mysql.database.azure.com}", "{unknownmind@ictc-obar}", "{#P@ssw0rd}", "{ictc_obar}", 3306);
+
 	mysqli_select_db($connect,"ictc_obar");
 	if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
